06bc08dd9d1d42af2a0a2c8251addac0e8712b3b
Mohammad Mahdi Hosseinzadeh <mohammadmahdihosseinzadeh68@gmail.com>
2026-05-31 18:09:46 +0330

    feat: add slider content and functionality, refactor links style and functionality

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 "use strict";
 
+// TODO: add scrolling behavior for all buttons
+
 const headerEl = document.querySelector("header");
 const navbarEl = document.querySelector(".navbar");
-const links = document.querySelectorAll(".navbar__link");
 const mobileNavbarButton = document.querySelector(".actions__button--mobile-nav");
 const heroSectionEl = document.querySelector(".hero");
 const scrollToTopButton = document.querySelector(".scrollToTop");
+const slides = document.querySelectorAll(".testimonials__slide");
+const testimonialsLeftButton = document.querySelector(".testimonials__btn--left");
+const testimonialsRightButton = document.querySelector(".testimonials__btn--right");
+const indicators = document.querySelector(".testimonials__indicators");
 
 const headerHeight = headerEl.offsetHeight;
 
@@ -19,10 +24,11 @@ mobileNavbarButton.addEventListener("click", function () {
   }
 });
 
-links.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+navbarEl.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("navbar__link")) {
+    const href = e.target.getAttribute("href");
 
     if (href === "#")
       window.scrollTo({
@@ -48,22 +54,19 @@ links.forEach(function (link) {
       navbarEl.classList.remove("mobile-navbar");
       document.body.classList.remove("overflow-y-hidden");
     }
-  });
+
+    e.target.blur();
+  }
 });
 
-const obs = new IntersectionObserver(
+const heroObserver = new IntersectionObserver(
   function (entries) {
-    const ent = entries[0];
+    const [entry] = entries;
+    const isHeroVisible = entry.isIntersecting;
 
-    if (ent.isIntersecting === false) {
-      headerEl.classList.add("sticky");
-      scrollToTopButton.classList.add("scrollToTop--visible");
-    }
+    headerEl.classList.toggle("sticky", !isHeroVisible);
 
-    if (ent.isIntersecting === true) {
-      headerEl.classList.remove("sticky");
-      scrollToTopButton.classList.remove("scrollToTop--visible");
-    }
+    scrollToTopButton.classList.toggle("scrollToTop--visible", !isHeroVisible);
   },
   {
     root: null,
@@ -71,8 +74,77 @@ const obs = new IntersectionObserver(
     rootMargin: `-${headerHeight}px`,
   },
 );
-obs.observe(heroSectionEl);
+heroObserver.observe(heroSectionEl);
 
 scrollToTopButton.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+const slider = function () {
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const createIndicators = function () {
+    slides.forEach(function (_, i) {
+      indicators.insertAdjacentHTML(
+        "beforeend",
+        `<button class="indicator" data-slide="${i}"></button>`,
+      );
+    });
+  };
+
+  const activateIndicator = function (slide) {
+    document
+      .querySelectorAll(".indicator")
+      .forEach((dot) => dot.classList.remove("indicator--active"));
+
+    document.querySelector(`.indicator[data-slide="${slide}"]`).classList.add("indicator--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+    activateIndicator(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateIndicator(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createIndicators();
+    activateIndicator(0);
+  };
+  init();
+
+  testimonialsLeftButton.addEventListener("click", prevSlide);
+  testimonialsRightButton.addEventListener("click", nextSlide);
+
+  document.addEventListener("keydown", function (e) {
+    e.key === "ArrowLeft" ? prevSlide() : nextSlide();
+  });
+
+  indicators.addEventListener("click", function (e) {
+    if (e.target.classList.contains("indicator")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateIndicator(slide);
+    }
+  });
+};
+slider();
